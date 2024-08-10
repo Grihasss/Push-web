9b59bdfb565f69491ee77f1d9d8034aed4deee29
Gregory <grihas56@mail.ru>
2024-08-10 13:46:59 +0300

    formatting

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,18 +1,15 @@
-self.addEventListener('push', event => {
-    console.log("PUSH EVENT")
-    let data = event.data.json();
-const image = 'https://cdn.glitch.com/614286c9-b4fc-4303-a6a9-a4cef0601b74%2Flogo.png?v=1605150951230';
-const options = {
-  body: data.options.body,
-  icon: image
-}
-self.registration.showNotification(
-  data.title, 
-  options
-);
+self.addEventListener("push", (event) => {
+  console.log("PUSH EVENT");
+  let data = event.data.json();
+  const image =
+    "https://cdn.glitch.com/614286c9-b4fc-4303-a6a9-a4cef0601b74%2Flogo.png?v=1605150951230";
+  const options = {
+    body: data.options.body,
+    icon: image,
+  };
+  self.registration.showNotification(data.title, options);
+});
 
-  });
-  
-  self.addEventListener("push", event => {
-    console.log("PUSH EVENT", event)
-  });
+self.addEventListener("push", (event) => {
+  console.log("PUSH EVENT", event);
+});
--- client/services/api.js
+++ client/services/api.js
@@ -1,33 +1,34 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8081',
-})
-
-export const subscribe = (subscription) => api.post('/add-subscription', 
-    JSON.stringify(subscription)
-  , {
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  })
-
-export const unsubscribe = (subscription) => api.post('/remove-subscription', 
-    JSON.stringify({endpoint: subscription.endpoint}) , {
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  })
-
-export const notify = (subscription) => api.post('/notify-me', 
-  JSON.stringify({endpoint: subscription.endpoint})
-,{
-  headers: {
-    'Content-Type': 'application/json'
-  },
-});
-const response = await fetch('/notify-all', {
-  method: 'POST'
+  baseURL: "http://localhost:8081",
 });
 
-export const notifyAll = () => api.post('/notify-all');
+export const subscribe = (subscription) =>
+  api.post("/add-subscription", JSON.stringify(subscription), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+});
+
+export const unsubscribe = (subscription) =>
+  api.post(
+    "/remove-subscription",
+    JSON.stringify({ endpoint: subscription.endpoint }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+});
+
+export const notify = (subscription) =>
+  api.post("/notify-me", JSON.stringify({ endpoint: subscription.endpoint }), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+const response = await fetch("/notify-all", {
+  method: "POST",
+});
+
+export const notifyAll = () => api.post("/notify-all");
